6a6d152b5b2a3bab116a595199be33838eaf6def
Ludovic Raess <ludovic.rass@gmail.com>
2022-07-20 13:43:31 +0200

    Cleanup

--- scripts/sia1D_bench_visnjevic18.jl
+++ scripts/sia1D_bench_visnjevic18.jl
@@ -22,11 +22,11 @@ end
     β0     = 0.01
     c      = 2.0
     # numerics
-    nx     = 512
+    nx     = 128*4
     nout   = 100
     ε      = 1e-2
     ε_nl   = 1e-8
-    damp   = 0.75
+    dmp    = 0.72
     itMax  = 1e5
     # preprocess
     a1     = 1.9e-24*ρg^npow*s2yr
@@ -35,18 +35,16 @@ end
     xc     = LinRange(-Lx/2+dx/2, Lx/2-dx/2, nx)
     cfl    = 1.0/2.1*dx*dx
     # initial
-    B      = zeros(nx  )
-    S      = zeros(nx  )
-    R      = zeros(nx  )
-    dHdτ   = zeros(nx  )
-    dτ     = zeros(nx  )
-    Err    = zeros(nx  )
-    Ela    = zeros(nx  )
-    β      = zeros(nx  )
+    B      = zeros(nx)
+    R      = zeros(nx)
+    dHdτ   = zeros(nx)
+    dτ     = zeros(nx)
+    Err    = zeros(nx)
+    Ela    = zeros(nx)
+    β      = zeros(nx)
     H      = 100.0*ones(nx)
     H[[1,end]] .= 0.0 
     @. B   = B0*exp(-xc*xc/1e10) + B0*exp.(-xc*xc/1e9)
-    @. S   = B
     # @. Ela = 2150 + 900
     @. Ela = 3000 + 400 * atan(xc/Lx)
     # @. β   = β0
@@ -55,9 +53,9 @@ end
     it = 1; err = 2*ε_nl
     while (err>ε_nl && it<itMax)
         residual!(R,H,B,Ela,β,dτ,c,npow,a1,a2,cfl,ε,dx)
-        @. dHdτ = dHdτ * damp + R
         @. Err  = H
-        @. H    = max(0.0, H + dτ * dHdτ)
+        @. dHdτ = dHdτ*dmp + R
+        @. H    = max(0.0, H + dτ*dHdτ)
         it += 1
         if it % nout == 0
             @. Err -= H
@@ -66,10 +64,9 @@ end
             if (err < ε_nl) break; end
         end
     end
-    @. S = B + H
-    Sp = copy(S); Sp[H.==0] .= NaN
-    p1 = plot(xc,[B,Sp], label=["Bed" "Surface"], linewidth=3)
-    p2 = plot(xc, H    , label="Ice thick", linewidth=3)
+    S  = B .+ H; S[H.==0] .= NaN
+    p1 = plot(xc,[B,S], label=["Bed" "Surface"], linewidth=3)
+    p2 = plot(xc, H   , label="Ice thick", linewidth=3)
     # p3 = plot(xc,β  , label="β", linewidth=3)
     p3 = plot(xc,Ela, label="ELA", linewidth=3)
     display(plot(p1,p2,p3, layout=(3, 1)))

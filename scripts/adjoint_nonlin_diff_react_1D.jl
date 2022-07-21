7bb58e794c39bb9766db67962844b3172588dbe6
Ivan Utkin <iutkin@ethz.ch>
2022-07-21 21:17:23 +0200

    Make it work

--- scripts/adjoint_nonlin_diff_react_1D.jl
+++ scripts/adjoint_nonlin_diff_react_1D.jl
@@ -1,12 +1,23 @@
 using Enzyme,Plots,Printf,LoopVectorization
 
-function residual!(R,H,B,β,npow,dx)
+function residual!(R,H,B,ELA,β,npow,dx)
     @inbounds @simd for ix = 2:length(R)-1
         ∇S_l  = (B[ix  ]-B[ix-1])/dx + (H[ix  ]-H[ix-1])/dx
         ∇S_r  = (B[ix+1]-B[ix  ])/dx + (H[ix+1]-H[ix  ])/dx
         D_l   = ((∇S_l>0.0)*(H[ix  ]^(npow+2) + H[ix  ]^npow) + (∇S_l<0.0)*(H[ix-1]^(npow+2) + H[ix-1]^npow))*abs(∇S_l)^2
         D_r   = ((∇S_r>0.0)*(H[ix+1]^(npow+2) + H[ix+1]^npow) + (∇S_r<0.0)*(H[ix  ]^(npow+2) + H[ix  ]^npow))*abs(∇S_r)^2
-        R[ix] = (D_r*∇S_r - D_l*∇S_l)/dx + min(β[ix]*(B[ix] + H[ix] - 0.2),0.01)
+        R[ix] = (D_r*∇S_r - D_l*∇S_l)/dx + min(β[ix]*(B[ix] + H[ix] - ELA[ix]),0.01)
+    end
+    return
+end
+
+function timestep!(dτ,H,B,npow,dx)
+    @inbounds @simd for ix = 2:length(H)-1
+        ∇S_l  = (B[ix  ]-B[ix-1])/dx + (H[ix  ]-H[ix-1])/dx
+        ∇S_r  = (B[ix+1]-B[ix  ])/dx + (H[ix+1]-H[ix  ])/dx
+        D_l   = ((∇S_l>0.0)*(H[ix  ]^(npow+2) + H[ix  ]^npow) + (∇S_l<0.0)*(H[ix-1]^(npow+2) + H[ix-1]^npow))*abs(∇S_l)^2
+        D_r   = ((∇S_r>0.0)*(H[ix+1]^(npow+2) + H[ix+1]^npow) + (∇S_r<0.0)*(H[ix  ]^(npow+2) + H[ix  ]^npow))*abs(∇S_r)^2
+        dτ[ix] = min(0.05, 0.05/(1e-5 + sqrt(0.5*(D_l + D_r))))
     end
     return
 end
@@ -20,28 +31,29 @@ function cost(H,H_obs)
 end
 
 mutable struct ForwardProblem{T<:Real,A<:AbstractArray{T}}
-    H::A; B::A; R::A; dR::A; β::A; npow::Int
+    H::A; B::A; ELA::A; R::A; dR::A; β::A; npow::Int
     niter::Int; ncheck::Int; ϵtol::T
     dx::T; dmp::T
 end
 
-function ForwardProblem(H,B,β,npow,niter,ncheck,ϵtol,dx,dmp)
+function ForwardProblem(H,B,ELA,β,npow,niter,ncheck,ϵtol,dx,dmp)
     R  = similar(H)
     dR = similar(H)
-    return ForwardProblem(H,B,R,dR,β,npow,niter,ncheck,ϵtol,dx,dmp)
+    return ForwardProblem(H,B,ELA,R,dR,β,npow,niter,ncheck,ϵtol,dx,dmp)
 end
 
 @views function solve!(problem::ForwardProblem)
-    (;H,B,R,dR,β,npow,niter,ncheck,ϵtol,dx,dmp) = problem
+    (;H,B,ELA,R,dR,β,npow,niter,ncheck,ϵtol,dx,dmp) = problem
     nx  = length(H)
-    dt  = dx/50
+    dτ  = zeros(nx)
     R  .= 0; dR .= 0
     merr = 2ϵtol; iter = 1
     while merr >= ϵtol && iter < niter
-        residual!(dR,H,B,β,npow,dx)
-        @. R = R*(1.0-dmp/nx) + dt*dR
+        residual!(dR,H,B,ELA,β,npow,dx)
+        timestep!(dτ,H,B,npow,dx)
+        @. R = R*(1.0-dmp/nx) + dτ*dR
         @. R[H == 0.0 && dR < 0.0] = 0.0
-        @. H = max(0.0, H + dt*R)
+        @. H = max(0.0, H + dτ*R)
         if iter % ncheck == 0
             merr = maximum(abs.(R))
             # display(plot(H))
@@ -57,36 +69,39 @@ end
 end
 
 mutable struct AdjointProblem{T<:Real,A<:AbstractArray{T}}
-    Ψ::A; R::A; dR; tmp1::A; tmp2::A; ∂J_∂H::A; H::A; H_obs::A; B::A; β::A; npow::Int
+    Ψ::A; R::A; dR; tmp1::A; tmp2::A; ∂J_∂H::A; H::A; H_obs::A; B::A; ELA::A; β::A; npow::Int
     niter::Int; ncheck::Int; ϵtol::T
     dx::T; dmp::T
 end
 
-function AdjointProblem(H,H_obs,B,β,npow,dx,dmp,niter,ncheck,ϵtol)
+function AdjointProblem(H,H_obs,B,ELA,β,npow,dx,dmp,niter,ncheck,ϵtol)
     Ψ     = similar(H)
     R     = similar(H)
     dR    = similar(H)
     tmp1  = similar(H)
     tmp2  = similar(H)
     ∂J_∂H = similar(H)
-    return AdjointProblem(Ψ,R,dR,tmp1,tmp2,∂J_∂H,H,H_obs,B,β,npow,niter,ncheck,ϵtol,dx,dmp)
+    return AdjointProblem(Ψ,R,dR,tmp1,tmp2,∂J_∂H,H,H_obs,B,ELA,β,npow,niter,ncheck,ϵtol,dx,dmp)
 end
 
 @views function solve!(problem::AdjointProblem)
-    (;Ψ,R,dR,tmp1,tmp2,∂J_∂H,H,H_obs,B,β,npow,niter,ncheck,ϵtol,dx,dmp) = problem
+    (;Ψ,R,dR,tmp1,tmp2,∂J_∂H,H,H_obs,B,ELA,β,npow,niter,ncheck,ϵtol,dx,dmp) = problem
     nx = length(Ψ)
-    dt = dx/3
+    dt = dx/5
     Ψ .= 0; R .= 0; dR .= 0
     @. ∂J_∂H = H - H_obs
     merr = 2ϵtol; iter = 1
     while merr >= ϵtol && iter < niter
         dR .= .-∂J_∂H; tmp2 .= Ψ
-        Enzyme.autodiff(residual!,Duplicated(tmp1,tmp2),Duplicated(H,dR),Const(B),Const(β),Const(npow),Const(dx))
+        Enzyme.autodiff(residual!,Duplicated(tmp1,tmp2),Duplicated(H,dR),Const(B),Const(ELA),Const(β),Const(npow),Const(dx))
         @. R  = R*(1.0-dmp/nx) + dt*dR
         @. Ψ += dt*R
+        R[H .== 0.0] .= 0.0
+        Ψ[H .== 0.0] .= 0.0
         Ψ[1] = 0; Ψ[end] = 0
         if iter % ncheck == 0
-            merr = maximum(abs.(dR[2:end-1]))
+            merr = maximum(abs.(R[2:end-1]))
+            # display(plot(Ψ))
             isfinite(merr) || error("adjoint solve failed") 
         end
         iter += 1
@@ -99,9 +114,9 @@ end
 end
 
 function cost_gradient!(Jn,problem::AdjointProblem)
-    (;Ψ,tmp1,tmp2,H,B,β,npow,dx) = problem
+    (;Ψ,tmp1,tmp2,H,B,ELA,β,npow,dx) = problem
     tmp1 .= .-Ψ; Jn .= 0.0
-    Enzyme.autodiff(residual!,Duplicated(tmp2,tmp1),Const(H),Const(B),Duplicated(β,Jn),Const(npow),Const(dx))
+    Enzyme.autodiff(residual!,Duplicated(tmp2,tmp1),Const(H),Const(B),Const(ELA),Duplicated(β,Jn),Const(npow),Const(dx))
     Jn[1] = Jn[2]; Jn[end] = Jn[end-1]
     return
 end
@@ -110,18 +125,18 @@ end
     # physics
     lx           = 100.0
     npow         = 3
-    β0           = 0.1
+    β0           = 0.25
     # numerics
     nx           = 128
     niter        = 1000nx
     ncheck       = 1nx
     ϵtol         = 1e-5
     gd_ϵtol      = 1e-3
-    dmp          = 0.6
-    dmp_adj      = 3/2
+    dmp          = 1.0
+    dmp_adj      = 1.5
     gd_niter     = 100
     bt_niter     = 10
-    γ0           = 1.0e-4
+    γ0           = 1.0e-2
     # preprocessing
     dx           = lx/nx
     xc           = LinRange(dx/2,lx-dx/2,nx)
@@ -131,14 +146,15 @@ end
     H[[1,end]]  .= 0.0
     H_obs        = copy(H)
     H_ini        = copy(H)
-    B            = 1.0 .* exp.(.-(xc./lx .-0.5).^2 ./ 0.25) .+ 2.5 .* exp.(.-(xc./lx .-0.5).^2 ./ 0.025)
-    β_synt       = β0 .* (1.0 .- 2.0 .* 10.0 .* abs.((2.0 .* xc/lx .- 1.0)./10.0))
-    β_ini        = 0.1 .* β_synt
+    B            = 1.0 .* exp.(.-(xc./lx .- 0.5).^2 ./ 0.25) .+ 2.5 .* exp.(.-(xc./lx .-0.5).^2 ./ 0.025)
+    ELA          = collect(2.0 .+ 0.5.*(xc./lx .- 0.5))
+    β_synt       = collect(β0 .- 0.015 .* atan.(xc./lx))
+    β_ini        = 0.4 .* β_synt
     β            = copy(β_ini)
     Jn           = zeros(nx) # cost function gradient
-    fwd_problem  = ForwardProblem(H,B,β,npow,niter,ncheck,ϵtol,dx,dmp)
-    adj_problem  = AdjointProblem(H,H_obs,B,β,npow,dx,dmp_adj,niter,ncheck,ϵtol)
-    synt_problem = ForwardProblem(H_obs,B,β_synt,npow,niter,ncheck,ϵtol,dx,dmp)
+    fwd_problem  = ForwardProblem(H,B,ELA,β,npow,niter,ncheck,ϵtol,dx,dmp)
+    adj_problem  = AdjointProblem(H,H_obs,B,ELA,β,npow,dx,dmp_adj,niter,ncheck,ϵtol)
+    synt_problem = ForwardProblem(H_obs,B,ELA,β_synt,npow,niter,ncheck,ϵtol,dx,dmp)
     # action
     println("  generating synthetic data...")
     solve!(synt_problem)
@@ -157,8 +173,8 @@ end
         # line search
         for bt_iter = 1:bt_niter
             @. β -= γ*Jn
-            for ism = 1:1
-                β[2:end-1] .+= 1/8 .* (β[1:end-2] - 2.0.*β[2:end-1] .+ β[3:end])
+            for ism = 1:5
+                β[2:end-1] .+= 1/5 .* (β[1:end-2] - 2.0.*β[2:end-1] .+ β[3:end])
             end
             fwd_problem.H .= H_ini
             solve!(fwd_problem)
@@ -172,20 +188,21 @@ end
                 γ *= 0.5
             end
         end
+        # visu
         push!(iter_evo,gd_iter); push!(J_evo,J_old)
+        @. S = B + H; S[H .== 0] .= NaN
+        p1 = plot(xc,[B,S,S_obs]; title="S"     , label=["B" "S" "S_obs"]      , ylim=(0,Inf), aspect_ratio=2, line = (2, [:solid :solid :dash]))
+        p2 = plot(xc,[β,β_synt] ; title="β"     , label=["current" "synthetic"], linewidth=2)
+        p3 = plot(xc,ELA        ; title="ELA"   , label=""                     , linewidth=2)
+        p4 = plot(iter_evo,J_evo; title="misfit", label=""                     , yaxis=:log10  ,linewidth=2)
+        display(plot(p1,p2,p3,p4;layout=(2,2),size=(980,980)))
+        # check convergence
         if J_old < gd_ϵtol
             @printf("  gradient descent converged, misfit = %.1e\n", J_old)
             break
         else
             @printf("  #iter = %d, misfit = %.1e\n", gd_iter, J_old)
         end
-        # visu
-        @. S = B + H; S[H .== 0] .= NaN
-        p1 = plot(xc,[B,S,S_obs]  ; title="S"   , label=["B" "H" "H_obs"]      , ylim=(0,Inf), aspect_ratio=2, line = (2, [:solid :solid :dash]))
-        p2 = plot(xc,[β,β_synt] ; title="β"     , label=["current" "synthetic"], linewidth=2)
-        p3 = plot(iter_evo,J_evo; title="misfit", label=""                     , yaxis=:log10  ,linewidth=2)
-        display(plot(p1,p2,p3;layout=(3,1),size=(600,980)))
-        sleep(0.1)
     end
     return
 end
